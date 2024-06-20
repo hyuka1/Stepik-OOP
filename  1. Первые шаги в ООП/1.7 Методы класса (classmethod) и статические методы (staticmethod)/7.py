15777afdbbd6785500f0120a5222056bbfee727b
Ruslan <hyuka0132@gmail.com>
2024-06-20 21:00:36 +0300

    Update solved problem 7

---  1. Первые шаги в ООП/1.7 Методы класса (classmethod) и статические методы (staticmethod)/7.py
+++  1. Первые шаги в ООП/1.7 Методы класса (classmethod) и статические методы (staticmethod)/7.py
@@ -22,22 +22,7 @@ class TextInput:
         return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
 
 
-class PasswordInput:
-    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
-    CHARS_CORRECT = CHARS + CHARS.upper() + digits
-
-    @classmethod
-    def check_name(cls, name):
-        if 3 <= len(name) <= 50 and (False not in (False if x not in cls.CHARS_CORRECT else True for x in name)):
-            return name
-        else:
-            raise ValueError("некорректное поле name")
-
-    def __init__(self, name, size=10):
-        self.name = name
-        self.size = size
-        self.check_name(self.name)
-
+class PasswordInput(TextInput):
     def get_html(self):
         return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
 
